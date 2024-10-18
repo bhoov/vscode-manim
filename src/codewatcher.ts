51931c8709d8c9d3b0a2fe062a0bc143dbc4b7a0
Splines <dominic-plein@gmx.de>
2024-10-18 18:13:19 +0200

    Rename static marker variable (regex)

--- src/codewatcher.ts
+++ src/codewatcher.ts
@@ -1,17 +1,16 @@
 import * as vscode from 'vscode';
 
 export class CodeWatcher implements vscode.CodeLensProvider, vscode.FoldingRangeProvider {
-    private static readonly PYTHON_CELL_MARKER = /^(##)/;
+    private static readonly MARKER = /^(##)/;
 
     constructor() { }
 
     public provideCodeLenses(document: vscode.TextDocument, token: vscode.CancellationToken): vscode.CodeLens[] {
         const codeLenses: vscode.CodeLens[] = [];
-        const regex = CodeWatcher.PYTHON_CELL_MARKER;
 
         for (let i = 0; i < document.lineCount; i++) {
             const line = document.lineAt(i);
-            if (regex.test(line.text)) {
+            if (CodeWatcher.MARKER.test(line.text)) {
                 const range = new vscode.Range(i, 0, i, line.text.length);
                 codeLenses.push(new vscode.CodeLens(range));
             }
@@ -31,12 +30,11 @@ export class CodeWatcher implements vscode.CodeLensProvider, vscode.FoldingRange
 
     public provideFoldingRanges(document: vscode.TextDocument, context: vscode.FoldingContext, token: vscode.CancellationToken): vscode.FoldingRange[] {
         const foldingRanges: vscode.FoldingRange[] = [];
-        const regex = CodeWatcher.PYTHON_CELL_MARKER;
         let start: number | null = null;
 
         for (let i = 0; i < document.lineCount; i++) {
             const line = document.lineAt(i);
-            if (regex.test(line.text)) {
+            if (CodeWatcher.MARKER.test(line.text)) {
                 if (start !== null) {
                     foldingRanges.push(new vscode.FoldingRange(start, i - 1));
                 }
