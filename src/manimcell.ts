4c0e0bae48a2da0cf090e1139c5308c97b819c7a
Splines <dominic-plein@gmx.de>
2024-10-18 18:30:48 +0200

    End folds when indentation level changes

--- src/manimcell.ts
+++ src/manimcell.ts
@@ -31,17 +31,28 @@ export class ManimCell implements vscode.CodeLensProvider, vscode.FoldingRangePr
     public provideFoldingRanges(document: vscode.TextDocument, context: vscode.FoldingContext, token: vscode.CancellationToken): vscode.FoldingRange[] {
         const foldingRanges: vscode.FoldingRange[] = [];
         let start: number | null = null;
+        let startIndent: number | null = null;
 
         for (let i = 0; i < document.lineCount; i++) {
             const line = document.lineAt(i);
+            const currentIndent = line.firstNonWhitespaceCharacterIndex;
+
             if (ManimCell.MARKER.test(line.text)) {
+                // Start a fold if the line is a marker
                 if (start !== null) {
                     foldingRanges.push(new vscode.FoldingRange(start, i - 1));
                 }
                 start = i;
+                startIndent = currentIndent;
+            } else if(start !== null && startIndent !== null && startIndent !== currentIndent) {
+                // End the fold if the indent level changes
+                foldingRanges.push(new vscode.FoldingRange(start, i - 1));
+                start = null;
+                startIndent = null;
             }
         }
 
+        // Last fold
         if (start !== null) {
             foldingRanges.push(new vscode.FoldingRange(start, document.lineCount - 1));
         }
