f63ef93703e4859d374f6321f80080070b67f218
Ben Hoover <24350185+bhoov@users.noreply.github.com>
2024-10-21 10:32:13 -0400

    Clean extension

--- src/extension.ts
+++ src/extension.ts
@@ -5,29 +5,20 @@ import * as vscode from 'vscode';
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscode-manim" is now active!');
-
-
-
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable1 = vscode.commands.registerCommand('vscode-manim.helloData', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		// terminal.show();
+	/** Example command
+	 * The command has been defined in the package.json file
+	 * The commandId parameter must match the command field in package.json
+	 */
+	const helloData = vscode.commands.registerCommand('vscode-manim.helloData', () => {
 		vscode.window.showInformationMessage('Hello Data from vscode-manim!');
 	});
 
 
-
-
+	/**
+	 * checkpoint_paste() functionality in a VSCode extension
+	 */
 	let isExecuting = false;  // Flag: to prevent several commands executing at the same time (because clipboard saving would become uncontrollable in this case)
-	const disposable2 = vscode.commands.registerCommand('vscode-manim.checkpointPaste', async () => {
+	const checkpointPaste = vscode.commands.registerCommand('vscode-manim.checkpointPaste', async () => {
 		if (isExecuting) {
 			vscode.window.showInformationMessage('Please wait until the current command finishes executing.');
 			return;
@@ -90,7 +81,7 @@ export function activate(context: vscode.ExtensionContext) {
 	});
 
 
-	context.subscriptions.push(disposable1, disposable2);
+	context.subscriptions.push(helloData, checkpointPaste);
 }
 
 // This method is called when your extension is deactivated
